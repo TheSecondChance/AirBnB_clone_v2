6b53b5a4719e62e597c6d2b9bc7df17b162950aa
eyasu <mesakmegerem@gmail.com>
2023-12-20 07:38:36 -0800

    Fix place

--- models/place.py
+++ models/place.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Float, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-import os
 
 place_amenity = Table("place_amenity", Base.metadata, Column(
     'place_id', String(60), ForeignKey('places.id'), nullable=False), Column(
@@ -28,10 +27,8 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship('Review', cascade='all, delete', backref='place')
 
-    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
-        reviews = relationship(
-            'Review', cascade='all, delete', backref='place')
-        amenities = relationship(
+    amenities = relationship(
             'Amenity', backref='place_amenities', secondary='place_amenity',
             viewonly=False)
