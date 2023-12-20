2557e0671441d529907c5aa6f075e79e2a186b00
eyasu <mesakmegerem@gmail.com>
2023-12-20 04:21:40 -0800

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -10,11 +10,12 @@ from models.review import Review
 from models.user import User
 
 
-if getenv('HBNB_TYPE_STORAGE', '') == 'db':
+stor = getenv("HBNB_TYPE_STORAGE")
+
+if stor == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
