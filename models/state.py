ada87df24cd2c65c2ac833c0874b73bc0dce0d20
eyasu <mesakmegerem@gmail.com>
2023-12-20 03:16:59 -0800

    Fix state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey, String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    sam = Column(String(128), nullable=False)
+    name = sam
+    ketm = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = ketm
