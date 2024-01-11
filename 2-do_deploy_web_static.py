fd7a65194c10555e00f2a7a62b074761229cd5e3
eyasu <mesakmegerem@gmail.com>
2024-01-11 10:16:38 -0800

    Fix 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,27 @@
 #!/usr/bin/python3
-"""
-distributes an archive to web servers
-using the function do_deploy"""
+"""Fabric script"""
 
-import os
-from fabric.api import *
-from datetime import datetime
-import shlex
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['52.91.154.49', '54.197.206.197']
-env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
+    """distributes web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        pname = archive_path.replace('/', ' ')
-        pname = shlex.split(pname)
-        pname = pname[-1]
-
-        wname = pname.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(pname)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        mezge = archive_path.split("/")[-1]
+        no_ext = mezge.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(mezge, path, no_ext))
+        run('rm /tmp/{}'.format(mezge))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except BaseException:
+    except:
         return False
