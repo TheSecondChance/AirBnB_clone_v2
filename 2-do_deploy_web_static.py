956fc9e8e83d551b717715cab054cfe5362c1b45
eyasu <mesakmegerem@gmail.com>
2024-01-11 04:37:27 -0800

    Add 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+distributes an archive to web servers
+using the function do_deploy"""
+
+import os
+from fabric.api import *
+from datetime import datetime
+import shlex
+env.hosts = ['52.91.154.49', '54.197.206.197']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        pname = archive_path.replace('/', ' ')
+        pname = shlex.split(pname)
+        pname = pname[-1]
+
+        wname = pname.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(pname)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except BaseException:
+        return False
