a861bf37b69d99a8135f555680c9c74abd684eb0
eyasu <mesakmegerem@gmail.com>
2024-01-16 10:48:29 -0800

    Fix 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-"""Fabric script"""
+"""
+Fabric script"""
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['52.91.154.49', '54.197.206.197']
+env.hosts = ['54.82.132.222', '52.91.148.76']
 
 
 def do_pack():
     """generates a tgz archive"""
     try:
-        zize = datetime.now().strftime("%Y%m%d%H%M%S")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        yzeSmem = "versions/web_static_{}.tgz".format(zize)
-        local("tar -cvzf {} web_static".format(yzeSmem))
-        return yzeSmem
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes archive web servers"""
+    """archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        yeSem = archive_path.split("/")[-1]
-        anoth = yeSem.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, anoth))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(yeSem, path, anoth))
-        run('rm /tmp/{}'.format(yeSem))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, anoth))
-        run('rm -rf {}{}/web_static'.format(path, anoth))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, anoth))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
 
 
 def deploy():
-    """distributes archive to the web servers"""
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
