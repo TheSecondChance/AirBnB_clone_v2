ffc44fa4b6a24dfbe35e0caff93a8da53b262de4
eyasu <mesakmegerem@gmail.com>
2024-01-16 04:11:00 -0800

    Fix 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "I'ts work" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
