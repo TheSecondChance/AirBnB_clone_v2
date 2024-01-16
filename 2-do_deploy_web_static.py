5fb7fe4fc8d1fe1b57d5fec54484ff6aeca407bf
eyasu <mesakmegerem@gmail.com>
2024-01-16 10:48:05 -0800

    Add 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Fabric script"""
+"""
+Fabric script
+"""
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['52.91.154.49', '54.197.206.197']
+env.hosts = ['54.82.132.222', '52.91.148.76']
 
 
 def do_deploy(archive_path):
-    """distributes web servers"""
+    """archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        mezge = archive_path.split("/")[-1]
-        no_ext = mezge.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(mezge, path, no_ext))
-        run('rm /tmp/{}'.format(mezge))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
@@ -25,3 +27,4 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
