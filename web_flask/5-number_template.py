8921cee36f260f59a8833895c9d9d50be61aff51
TheSecondChance <mesakmegerem@gmail.com>
2024-01-26 11:33:57 -0800

    Add 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""A script flask web application
+listening on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return string"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbn():
+    """Return string"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def anot(text='is cool'):
+    """python display the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Disblay if n is number"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def priNumb(n):
+    """print number
+    """
+    return render_template("5-number.html", num=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
