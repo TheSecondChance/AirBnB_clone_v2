90eb0fdb8b34ae0eea2f111d2607230c1e64d6b9
TheSecondChance <mesakmegerem@gmail.com>
2024-01-26 21:01:24 -0800

    Fix 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""A script flask web application
-listening on 0.0.0.0, port 5000"""
+"""
+A script flask web application
+listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask, render_template
 
@@ -9,21 +11,24 @@ app = Flask("__name__")
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return string
+    """
+    Return string
     """
     return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbn():
-    """Return string
+    """
+    Return string
     """
     return ("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """followed by the value of the text variable
+    """
+    followed by the value of the text variable
     """
     return 'C ' + text.replace('_', ' ')
 
@@ -31,14 +36,16 @@ def cisfun(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def anot(text='is cool'):
-    """python display the value of the text variable
+    """
+    python display the value of the text variable
     """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Disblay if n is number
+    """
+    Disblay if n is number
     """
     return "{} is a number".format(n)
 
@@ -46,7 +53,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_temp(n):
     """
-        template path to display
+    template path to display
     """
     return render_template('templates/5-number.html', n=n)
 
