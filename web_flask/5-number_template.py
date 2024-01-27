5a8fa3452e49b30864d4c94d63346da601635706
TheSecondChance <mesakmegerem@gmail.com>
2024-01-26 20:53:12 -0800

    Fix 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,32 +9,37 @@ app = Flask("__name__")
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return string"""
+    """Return string
+    """
     return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbn():
-    """Return string"""
+    """Return string
+    """
     return ("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """followed by the value of the text variable"""
+    """followed by the value of the text variable
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def anot(text='is cool'):
-    """python display the value of the text variable"""
+    """python display the value of the text variable
+    """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Disblay if n is number"""
+    """Disblay if n is number
+    """
     return "{} is a number".format(n)
 
 
