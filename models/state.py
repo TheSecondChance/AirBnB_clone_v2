fe48d8a9222beca62fd1aaded559d4f870812114
TheSecondChance <mesakmegerem@gmail.com>
2024-01-28 10:53:25 -0800

    Fix state.py

--- models/state.py
+++ models/state.py
@@ -13,18 +13,18 @@ from os import getenv
 class State(BaseModel, Base):
     """"state a for many to many relationship table
     between Place and Amenity record"""
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="stats", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
         def cities(self):
             """a for many to many relationship table
             between Place and Amenity record"""
             city_list = []
-            for city in models.storage.al(City).values():
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
