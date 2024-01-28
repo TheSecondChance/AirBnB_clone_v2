c530bda5d0632844be74c16aaebda32a02b62a83
TheSecondChance <mesakmegerem@gmail.com>
2024-01-28 11:00:35 -0800

    Fix city.py

--- models/city.py
+++ models/city.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""""state a for many to many relationship table
+between Place and Amenity record
+"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from uuid import uuid4
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(
-            String(60),
-            ForeignKey('states.id'),
-            nullable=False)
-    places = relationship(
-            'Place',
-            backref='cities',
-            cascade='all, delete-orphan')
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
