8ae7dbfb7d9310e857aa2824a3c91e750736141a
TheSecondChance <mesakmegerem@gmail.com>
2024-01-28 10:58:24 -0800

    Fix state.py

--- models/state.py
+++ models/state.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-"""stat class
+""""state a for many to many relationship table
+between Place and Amenity record
 """
-
-import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
+import models
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """"state a for many to many relationship table
-    between Place and Amenity record"""
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
+    between Place and Amenity record
+    """
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
+        name = ""
+
+    if storage_type != 'db':
         @property
         def cities(self):
-            """a for many to many relationship table
-            between Place and Amenity record"""
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """
+            get list of City instances with state_id
+            equals to the current State.id
+            """
+            list_cities = []
+            all_cities = models.storage.all(City)
+            for key, city_obj in all_cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
+            return list_cities
