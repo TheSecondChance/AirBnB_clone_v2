b48e4d9b8a7eea9f34ebe89d83697e6aa696f0fc
TheSecondChance <mesakmegerem@gmail.com>
2024-01-31 04:43:20 -0800

    Add 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,13 +5,23 @@ script Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove SQLAlchemy Session
+    after each request
+    Remove SQLAlchemy Session
+    after each request
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
 def state_list():
     """this for display a HTML page: inside the tag <BODY>
     tis for display a HTML page: inside the tag <BODY>
@@ -21,20 +31,9 @@ def state_list():
     <UL> tag: 'List of all State objects present in DBStorage
     <LI> tag: description of one State
     """
-    states = storage.all(State).values()
-
+    states = sorted(storage.all(State).values(), key=lambda a: a.name)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(exceptions):
-    """Remove SQLAlchemy Session
-    after each request
-    Remove SQLAlchemy Session
-    after each request
-    """
-    storage.close()
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000')
